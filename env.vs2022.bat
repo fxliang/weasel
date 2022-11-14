06f690a4a62bc718e7f540149b452fa5baf160d6
liangfx <fx.liang@outlook.com>
2022-11-14 13:43:47 +0800

    delete useless file, and sync env.vs2019.bat and env.vs2022.bat

--- env.vs2022.bat
+++ env.vs2022.bat
@@ -3,7 +3,7 @@ rem Customize your build environment and save the modified copy to env.bat
 set WEASEL_ROOT=%CD%
 
 rem REQUIRED: path to Boost source directory
-if not defined BOOST_ROOT set BOOST_ROOT=%WEASEL_ROOT%\deps\boost_1_76_0
+if not defined BOOST_ROOT set BOOST_ROOT=%WEASEL_ROOT%\deps\boost_1_78_0
 
 rem OPTIONAL: architecture, Visual Studio version and platform toolset
 set ARCH=Win32
@@ -12,4 +12,4 @@ set CMAKE_GENERATOR="Visual Studio 17 2022"
 set PLATFORM_TOOLSET=v143
 
 rem OPTIONAL: path to additional build tools
-rem set DEVTOOLS_PATH=%ProgramFiles%\Git\cmd;%ProgramFiles%\CMake\bin;C:\Python27;
+rem set DEVTOOLS_PATH=%ProgramFiles%\Git\cmd;%ProgramFiles%\Git\usr\bin;%ProgramFiles%\CMake\bin;C:\Python27;
