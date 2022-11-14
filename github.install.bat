982454ffc1996187e79145894dea988d189bcc36
fxliang <fx.liang@outlook.com>
2022-11-14 15:30:40 +0800

    Update github.install.bat, resolve conflict

--- github.install.bat
+++ github.install.bat
@@ -1,4 +1,3 @@
-<<<<<<<< HEAD:ci.install.bat
 setlocal
 
 git submodule init
@@ -15,21 +14,4 @@ copy /Y librime\dist\lib\rime.lib lib\
 copy /Y librime\dist\lib\rime.dll output\
 if not exist output\data\opencc mkdir output\data\opencc
 copy /Y librime\thirdparty\share\opencc\*.* output\data\opencc\
-========
-setlocal
 
-git submodule init
-git submodule update plum
-
-set rime_version=1.5.3
-set rime_variant=rime-with-plugins
-
-set download_archive=%rime_variant%-%rime_version%-win32.zip
-curl -LO https://github.com/rime/librime/releases/download/%rime_version%/%download_archive%
-7z x %download_archive% * -olibrime\ | find "ing archive"
-copy /Y librime\dist\include\rime_*.h include\
-copy /Y librime\dist\lib\rime.lib lib\
-copy /Y librime\dist\lib\rime.dll output\
-if not exist output\data\opencc mkdir output\data\opencc
-copy /Y librime\thirdparty\share\opencc\*.* output\data\opencc\
->>>>>>>> CI_test:github.install.bat
