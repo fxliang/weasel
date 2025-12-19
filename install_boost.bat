e6581c8250e9e8d510f5a317c466205a1641e964
fxliang <fx.liang@outlook.com>
2025-12-19 17:44:31 +0800

    fix: install_boost.bat deps dir

--- install_boost.bat
+++ install_boost.bat
@@ -7,6 +7,7 @@ set boost_x_y_z=%boost_version:.=_%
 
 if not defined BOOST_ROOT set BOOST_ROOT=%RIME_ROOT%\deps\boost_%boost_x_y_z%
 
+if not exist "%RIME_ROOT%\deps" ( mkdir %RIME_ROOT%\deps )
 if exist "%BOOST_ROOT%\boost" goto boost_found
 for %%I in ("%BOOST_ROOT%\.") do set src_dir=%%~dpI
 rem download boost source with powershell
